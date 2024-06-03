00d0020b03632c6df3770afe5266f07275b11f0f
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-06-03 22:07:16 +0530

    kanban columns setup

---
+++ dnd-kanban/src/components/ColumonContainer.tsx
@@ -0,0 +1,72 @@
+import PlusIcon from "../icons/PlusIcon";
+import TrashIcon from "../icons/TrashIcon";
+import { Column, Id, Task } from "../types";
+
+interface Props {
+  column: Column;
+  deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
+
+  createTask: (columnId: Id) => void;
+  updateTask: (id: Id, content: string) => void;
+  deleteTask: (id: Id) => void;
+  tasks: Task[];
+}
+
+function ColumnContainer(props: Props) {
+  const { column } = props;
+  return (
+    <div className="bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+        {/* Column title */}
+      <div
+        onClick={() => {
+        }}
+        className=" bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3
+      font-bold border-columnBackgroundColor border-4 flex items-center justify-between"
+>
+        <div className="flex gap-2">
+          <div
+            className=" flex justify-center items-center bg-columnBackgroundColor px-2 py-1 text-sm rounded-full"
+          >
+            0
+          </div>
+            <input
+              className="bg-black focus:border-rose-500 border rounded outline-none px-2"
+              value={column.title}
+              onChange={(e) =>}
+              autoFocus
+              onBlur={() => {
+                // setEditMode(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key !== "Enter") return;
+              }}
+            />
+        </div>
+        <button
+          onClick={() => {
+          }}
+          className=" stroke-gray-500 hover:stroke-white hover:bg-columnBackgroundColor rounded px-1 py-2"
+        >
+          <TrashIcon />
+        </button>
+      </div>
+
+      {/* Column task container */}
+      <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
+        
+      </div>
+      {/* Column footer */}
+      <button
+        className="flex gap-2 items-center border-columnBackgroundColor border-2 rounded-md p-4 border-x-columnBackgroundColor hover:bg-mainBackgroundColor hover:text-rose-500 active:bg-black"
+        onClick={() => {
+        }}
+      >
+        <PlusIcon />
+        Add task
+      </button>
+    </div>
+  )
+}
+
+export default ColumnContainer;
